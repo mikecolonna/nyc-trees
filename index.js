350bcb03dfb473972f7288505f11ff6eb0d334c7
Ellie Czepiel <ellieczepiel@Ellies-MacBook-Pro.local>
2018-01-03 15:17:17 -0500

    sorted by borough

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ async function getTrees() {
     try {
         let response = await fetch("https://data.cityofnewyork.us/resource/5rq2-4hqu.json")
         let trees = await response.json()
-        renderTrees(trees)
-        console.log(trees)
+        let boroughAnswer = document.getElementById('borough').value
+        renderTrees(parseTrees(trees, boroughAnswer))
+        //console.log(trees)
     } catch (error) {
         console.log(error)
     }
 }
 
-parseTrees(trees, parameters) {
-    
+function parseTrees(trees, borough) {
+  /* this works but looks bad
+  for (i = 0; i < 1000; i++){
+    let currentTree = trees[i]
+    if (!(currentTree.boroname == borough)){
+      delete result[i]
+    }
+  }*/
+  let result = trees.filter(tree => tree.boroname == borough)
+  console.log(result)
+  return result
 }
 
-renderTrees(trees) {
+function renderTrees(trees) {
 
 }
 
